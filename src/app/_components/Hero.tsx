8f6eae8cb807cbae5c0afef3fa3e2861e514f27e
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-07-10 14:38:04 +0100

    t

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -2,33 +2,37 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="text-foreground py-12 flex flex-col items-center justify-center gap-6 p-6 md:gap-8">
-      <div className="flex flex-col items-center justify-center gap-4 text-center w-full md:gap-8">
-        <p className="text-sm text-center max-w-3xl text-balance text-foreground/60 md:text-xl">
-          We only know how to{" "}
-          <span className="text-foreground">Build Websites</span>
-        </p>
-        <h1 className="text-6xl text-center  max-w-3xl font-bold leading-tight  md:text-8xl tracking-tighter">
-          But we are *** good at it
+    <section className="text-foreground py-8 flex flex-col items-center justify-center gap-6 p-6 md:gap-8">
+      <div className="flex flex-col items-center justify-center  text-center w-full max-w-3xl ">
+        <div className="flex flex-col gap-4 border-x border-foreground/20 border-dashed p-4 mb-[2px] w-full">
+          <p className="text-sm text-center max-w-3xl text-balance text-foreground/60 md:text-xl">
+            We only know how to{" "}
+            <span className="text-foreground">Build Websites</span>
+          </p>
+        </div>
+
+        <h1 className="text-5xl text-center  max-w-3xl font-bold leading-tight font-bold  md:text-8xl tracking-tighter text-balance  border p-4 border-foreground/20 border-dashed">
+          But we are **** good at it
         </h1>
+        <div className="flex flex-col gap-4 border-x border-foreground/20 border-dashed p-4 mt-[2px] w-full max-w-3xl ">
+          <p className="text-center text-sm text-foreground/60 md:text-xl leading-loose max-w-2xl text-balance   ">
+            Create quality, fast and full customs web sites to your wildest
+            imagination.
+          </p>
 
-        <p className="text-center text-sm text-foreground/60 md:text-xl leading-loose max-w-2xl text-balance   ">
-          Create quality, fast and full customs web sites to your wildest
-          imagination.
-        </p>
-      </div>
-
-      <div className="flex flex-row items-center justify-center gap-4  w-full max-w-md mx-auto">
-        <Link href="/calculate" className="w-full">
-          <button className="px-4 py-2 w-full border rounded bg-foreground text-background hover:bg-background hover:text-foreground border-foreground md:text-lg">
-            Calculate Now
-          </button>
-        </Link>
-        <Link href="/portfolio" className="w-full">
-          <button className="px-4 py-2 w-full border rounded border-foreground/20 hover:border-foreground hover:text-foreground md:text-lg">
-            Explore
-          </button>
-        </Link>
+          <div className="flex flex-row items-center justify-center gap-4  w-full max-w-md mx-auto ">
+            <Link href="/calculate" className="w-full">
+              <button className="px-4 py-2 w-full border rounded bg-foreground text-background hover:bg-background hover:text-foreground border-foreground md:text-lg">
+                Calculate Now
+              </button>
+            </Link>
+            <Link href="/portfolio" className="w-full">
+              <button className="px-4 py-2 w-full border rounded border-foreground/20 hover:border-foreground hover:text-foreground md:text-lg">
+                Explore
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   );
