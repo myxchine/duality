839d48fcb19930923f1a3ffc6f6792c1df8750c9
Michael Dos Santos <michael.dozzantoz@gmail.com>
2025-03-31 02:01:21 +0100

    new email because other is blocked

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Michael Santos",
   url: "https://michaelsantos.pt",
-  email: "michael@michaelsantos.pt",
+  email: "michael.dozzantoz@gmail.com",
 };
 export const mainMenu = [
   { name: "Home", href: "/" },
